bdec70735b435db9c9e49c2388c2b24af798206b
Ikantra <sephirothowa@hotmail.com>
2018-11-22 11:11:26 +0100

    test

--- powershell/git.ps1
+++ powershell/git.ps1
@@ -2,7 +2,10 @@
 
 $myRepo = "http://www.github.com/ikantra/scripts"
 
-#
+function initGit($uName, $eMail){
+    git config --global user.name $uName
+    git config --global user.email $eMail
+}
 
 #Copy from branch
 git clone $myRepo
